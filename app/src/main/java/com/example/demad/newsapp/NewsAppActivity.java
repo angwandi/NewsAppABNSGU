54fcd809cfbf82076751412d8cd0592d13015cf8
Angwandi <demsdema217@gmail.com>
2018-05-10 11:52:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/demad/newsapp/NewsAppActivity.java
@@ -0,0 +1,11 @@
+package com.example.demad.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class NewsAppActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_app);
+    }
+}
